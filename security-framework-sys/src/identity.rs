bb866e681f976c6e69cbd45862b295dd10566ed1
Steven Fackler <sfackler@gmail.com>
2015-09-19 22:12:01 -0700

    Certificate and private key accessors for identity

--- security-framework-sys/src/identity.rs
+++ security-framework-sys/src/identity.rs
@@ -1,5 +1,11 @@
-use core_foundation_sys::base::CFTypeID;
+use core_foundation_sys::base::{OSStatus, CFTypeID};
+
+use base::{SecCertificateRef, SecKeyRef, SecIdentityRef};
 
 extern {
     pub fn SecIdentityGetTypeID() -> CFTypeID;
+    pub fn SecIdentityCopyCertificate(identity: SecIdentityRef,
+                                      certificate_ref: *mut SecCertificateRef) -> OSStatus;
+    pub fn SecIdentityCopyPrivateKey(identity: SecIdentityRef,
+                                     key_ref: *mut SecKeyRef) -> OSStatus;
 }
--- security-framework/src/identity.rs
+++ security-framework/src/identity.rs
@@ -1,8 +1,14 @@
 use core_foundation_sys::base::CFRelease;
 use core_foundation::base::TCFType;
 use security_framework_sys::base::SecIdentityRef;
-use security_framework_sys::identity::{SecIdentityGetTypeID};
+use security_framework_sys::identity::*;
 use std::mem;
+use std::ptr;
+
+use cvt;
+use base::Result;
+use certificate::SecCertificate;
+use key::SecKey;
 
 pub struct SecIdentity(SecIdentityRef);
 
@@ -13,3 +19,39 @@ impl Drop for SecIdentity {
 }
 
 impl_TCFType!(SecIdentity, SecIdentityRef, SecIdentityGetTypeID);
+
+impl SecIdentity {
+    pub fn certificate(&self) -> Result<SecCertificate> {
+        unsafe {
+            let mut certificate = ptr::null_mut();
+            try!(cvt(SecIdentityCopyCertificate(self.0, &mut certificate)));
+            Ok(SecCertificate::wrap_under_create_rule(certificate))
+        }
+    }
+
+    pub fn private_key(&self) -> Result<SecKey> {
+        unsafe {
+            let mut key = ptr::null_mut();
+            try!(cvt(SecIdentityCopyPrivateKey(self.0, &mut key)));
+            Ok(SecKey::wrap_under_create_rule(key))
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use test::identity;
+
+    #[test]
+    fn certificate() {
+        let identity = identity();
+        let certificate = p!(identity.certificate());
+        assert_eq!("foobar.com", p!(certificate.common_name()).to_string());
+    }
+
+    #[test]
+    fn private_key() {
+        let identity = identity();
+        p!(identity.private_key());
+    }
+}
