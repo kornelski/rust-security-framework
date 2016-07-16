9eb1616948bf6b29e83b03ee01e84aed9adb6dc6
Steven Fackler <sfackler@palantir.com>
2016-07-16 19:08:50 +0100

    Release v0.1.6

--- security-framework-sys/src/lib.rs
+++ security-framework-sys/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.5")]
+#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.6")]
 #![allow(non_upper_case_globals)]
 
 extern crate core_foundation_sys;
