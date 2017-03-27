291c18c7bfe0ae6ea25d1aa412ba65de203aab95
Steven Fackler <sfackler@gmail.com>
2017-03-26 22:02:13 -0700

    Release v0.1.13

--- security-framework-sys/src/lib.rs
+++ security-framework-sys/src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.12")]
+#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.13")]
 #![allow(non_upper_case_globals)]
 
 extern crate core_foundation_sys;
--- security-framework/src/lib.rs
+++ security-framework/src/lib.rs
@@ -1,6 +1,6 @@
 //! Wrappers around the OSX Security Framework.
 
-#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.12")]
+#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.13")]
 #![warn(missing_docs)]
 #![allow(non_upper_case_globals)]
 
