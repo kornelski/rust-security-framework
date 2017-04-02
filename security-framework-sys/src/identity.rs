4ba1d689ed4ec82e2f41cd4e82682b1094a10e67
Steven Fackler <sfackler@gmail.com>
2017-04-01 22:09:29 -0700

    Support identity creation

--- security-framework-sys/src/identity.rs
+++ security-framework-sys/src/identity.rs
@@ -1,4 +1,4 @@
-use core_foundation_sys::base::{OSStatus, CFTypeID};
+use core_foundation_sys::base::{OSStatus, CFTypeID, CFTypeRef};
 
 use base::{SecCertificateRef, SecKeyRef, SecIdentityRef};
 
@@ -10,4 +10,9 @@ extern "C" {
     pub fn SecIdentityCopyPrivateKey(identity: SecIdentityRef,
                                      key_ref: *mut SecKeyRef)
                                      -> OSStatus;
+    #[cfg(target_os = "macos")]
+    pub fn SecIdentityCreateWithCertificate(keychain_or_Array: CFTypeRef,
+                                            certificate_ref: SecCertificateRef,
+                                            identity_ref: *mut SecIdentityRef)
+                                            -> OSStatus;
 }
