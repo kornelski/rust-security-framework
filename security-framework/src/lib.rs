ca9d9dc9089e901bb4b375ce0c38352f7b0f5e5b
Steven Fackler <sfackler@gmail.com>
2017-08-14 20:42:29 -0700

    Release v0.1.16

--- security-framework/src/lib.rs
+++ security-framework/src/lib.rs
@@ -1,6 +1,6 @@
 //! Wrappers around the OSX Security Framework.
 
-#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.14")]
+#![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.16")]
 #![warn(missing_docs)]
 #![allow(non_upper_case_globals)]
 
