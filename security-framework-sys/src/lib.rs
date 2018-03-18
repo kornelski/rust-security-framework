a1bbf87591ebb7c031c376dc7b5e94db76cabe1e
Steven Fackler <sfackler@palantir.com>
2018-03-18 14:45:51 -0700

    Ignore more warnings in sys crate

--- security-framework-sys/src/lib.rs
+++ security-framework-sys/src/lib.rs
@@ -1,5 +1,5 @@
 #![doc(html_root_url = "https://sfackler.github.io/rust-security-framework/doc/v0.1.14")]
-#![allow(non_upper_case_globals)]
+#![allow(bad_style)]
 
 extern crate core_foundation_sys;
 extern crate libc;
