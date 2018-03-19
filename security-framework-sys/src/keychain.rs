0248d04e2b5abc247350fb2b6e5a16b2ae8ac8c7
Steven Fackler <sfackler@palantir.com>
2018-03-18 18:15:57 -0700

    Don't use u32

--- security-framework-sys/src/keychain.rs
+++ security-framework-sys/src/keychain.rs
@@ -1,16 +1,16 @@
 use core_foundation_sys::base::{Boolean, OSStatus, CFTypeID};
-use libc::{c_char, c_void};
+use libc::{c_char, c_int, c_void};
 
 use base::{SecAccessRef, SecKeychainRef};
 
-pub const SEC_KEYCHAIN_SETTINGS_VERS1: u32 = 1;
+pub const SEC_KEYCHAIN_SETTINGS_VERS1: c_int = 1;
 
 #[repr(C)]
 pub struct SecKeychainSettings {
-    pub version: u32,
+    pub version: c_int,
     pub lockOnSleep: Boolean,
     pub useLockInterval: Boolean,
-    pub lockInterval: u32,
+    pub lockInterval: c_int,
 }
 
 extern "C" {
@@ -19,7 +19,7 @@ extern "C" {
     pub fn SecKeychainCopyDefault(keychain: *mut SecKeychainRef) -> OSStatus;
     #[cfg(target_os = "macos")]
     pub fn SecKeychainCreate(pathName: *const c_char,
-                             passwordLength: u32,
+                             passwordLength: c_int,
                              password: *const c_void,
                              promptUser: Boolean,
                              initialAccess: SecAccessRef,
@@ -29,7 +29,7 @@ extern "C" {
     pub fn SecKeychainOpen(pathName: *const c_char, keychain: *mut SecKeychainRef) -> OSStatus;
     #[cfg(target_os = "macos")]
     pub fn SecKeychainUnlock(keychain: SecKeychainRef,
-                             passwordLength: u32,
+                             passwordLength: c_int,
                              password: *const c_void,
                              usePassword: Boolean)
                              -> OSStatus;
