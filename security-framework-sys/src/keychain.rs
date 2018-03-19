c3526134736705bc7be5f65a780af9ec4664d2ed
Steven Fackler <sfackler@palantir.com>
2018-03-18 18:29:12 -0700

    Unsigned, not signed

--- security-framework-sys/src/keychain.rs
+++ security-framework-sys/src/keychain.rs
@@ -1,23 +1,23 @@
 use core_foundation_sys::base::{Boolean, OSStatus, CFTypeID};
-use libc::{c_char, c_int, c_void};
+use libc::{c_char, c_uint, c_void};
 
 use base::{SecAccessRef, SecKeychainRef};
 
-pub const SEC_KEYCHAIN_SETTINGS_VERS1: c_int = 1;
+pub const SEC_KEYCHAIN_SETTINGS_VERS1: c_uint = 1;
 
 #[repr(C)]
 pub struct SecKeychainSettings {
-    pub version: c_int,
+    pub version: c_uint,
     pub lockOnSleep: Boolean,
     pub useLockInterval: Boolean,
-    pub lockInterval: c_int,
+    pub lockInterval: c_uint,
 }
 
 extern "C" {
     pub fn SecKeychainGetTypeID() -> CFTypeID;
     pub fn SecKeychainCopyDefault(keychain: *mut SecKeychainRef) -> OSStatus;
     pub fn SecKeychainCreate(pathName: *const c_char,
-                             passwordLength: c_int,
+                             passwordLength: c_uint,
                              password: *const c_void,
                              promptUser: Boolean,
                              initialAccess: SecAccessRef,
@@ -25,7 +25,7 @@ extern "C" {
                              -> OSStatus;
     pub fn SecKeychainOpen(pathName: *const c_char, keychain: *mut SecKeychainRef) -> OSStatus;
     pub fn SecKeychainUnlock(keychain: SecKeychainRef,
-                             passwordLength: c_int,
+                             passwordLength: c_uint,
                              password: *const c_void,
                              usePassword: Boolean)
                              -> OSStatus;
