4fb70fb76e06abe2698a9e5de109d01fd526b822
Ragoso <matheusragoso@gmail.com>
2023-05-16 18:45:17 -0300

    limit on errSSLPeerAuthCompleted

--- security-framework/src/secure_transport.rs
+++ security-framework/src/secure_transport.rs
@@ -1143,8 +1143,8 @@ impl<S: Read + Write> Read for SslStream<S> {
 
             match ret {
                 errSSLClosedGraceful | errSSLClosedAbort | errSSLClosedNoNotify => Ok(0),
-                // this errors are not fatal, we can continue reading
-                errSSLPeerAuthCompleted | errSSLClientCertRequested | errSSLWouldBlock => self.read(buf),
+                // this error isn't fatal
+                errSSLPeerAuthCompleted => self.read(buf),
                 _ => Err(self.get_error(ret)),
             }
         }
