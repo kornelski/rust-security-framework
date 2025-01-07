433f35e126c0b6c691074bc2bb84f7ce325234fe
Kornel <kornel@geekhood.net>
2025-01-07 15:16:56 +0000

    Simplify cfg boilerplate

--- security-framework/src/lib.rs
+++ security-framework/src/lib.rs
@@ -1,3 +1,5 @@
+#![cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
+
 //! Wrappers around the OSX Security Framework.
 #![warn(missing_docs)]
 #![allow(non_upper_case_globals)]
@@ -31,28 +33,20 @@ pub mod access_control;
 #[cfg(target_os = "macos")]
 pub mod authorization;
 pub mod base;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod certificate;
 pub mod cipher_suite;
 #[cfg(target_os = "macos")]
 pub mod cms;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod identity;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod import_export;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod item;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod key;
 pub mod os;
 pub mod passwords;
 pub mod passwords_options;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod policy;
 pub mod random;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod secure_transport;
-#[cfg(any(target_os = "macos", target_os = "ios", target_os = "tvos", target_os = "watchos", target_os = "visionos"))]
 pub mod trust;
 #[cfg(target_os = "macos")]
 pub mod trust_settings;
