9152beeb93001f6980b0a563cb85d8b88dd6096e
Soham Zemse <22412996+zemse@users.noreply.github.com>
2025-02-20 14:57:59 +0530

    expose simplify dict logic

--- security-framework/src/item.rs
+++ security-framework/src/item.rs
@@ -556,31 +556,7 @@ impl SearchResult {
     #[must_use]
     pub fn simplify_dict(&self) -> Option<HashMap<String, String>> {
         match *self {
-            Self::Dict(ref d) => unsafe {
-                let mut retmap = HashMap::new();
-                let (keys, values) = d.get_keys_and_values();
-                for (k, v) in keys.iter().zip(values.iter()) {
-                    let keycfstr = CFString::wrap_under_get_rule((*k).cast());
-                    let val: String = match CFGetTypeID(*v) {
-                        cfstring if cfstring == CFString::type_id() => {
-                            format!("{}", CFString::wrap_under_get_rule((*v).cast()))
-                        },
-                        cfdata if cfdata == CFData::type_id() => {
-                            let buf = CFData::wrap_under_get_rule((*v).cast());
-                            let mut vec = Vec::new();
-                            vec.extend_from_slice(buf.bytes());
-                            format!("{}", String::from_utf8_lossy(&vec))
-                        }
-                        cfdate if cfdate == CFDate::type_id() => format!(
-                            "{}",
-                            CFString::wrap_under_create_rule(CFCopyDescription(*v))
-                        ),
-                        _ => String::from("unknown"),
-                    };
-                    retmap.insert(format!("{keycfstr}"), val);
-                }
-                Some(retmap)
-            },
+            Self::Dict(ref d) => Some(simplify_dict(d)),
             _ => None,
         }
     }
